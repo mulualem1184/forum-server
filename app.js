6d54a8ec4a0b9a90157aa28012d97ce0bf5f2b8c
mul <jimpowerdire@gmail.com>
2025-01-10 15:08:24 +0300

    update 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express=require('express')
 const app=express();
 const cors=require('cors')
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 
 // database connection
