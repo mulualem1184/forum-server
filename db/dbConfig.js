09a63abcecf3bd14a62b055370ce80f8ffaa7c61
mul <jimpowerdire@gmail.com>
2025-01-12 14:05:40 +0300

    updat2

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,11 +6,11 @@ const dbconnection =mysql2.createPool({
 //   host:"localhost",
 //   password:process.env.PASSWORD,
 //   connectionLimit:10  
-host: process.env.DATABASE_HOST ||'begeifmysnlnhnz9shw9-mysql.services.clever-cloud.com', // Database host
-user: process.env.DATABASE_USER ||'u4bz25p8gzp7tudf',                                     // Database user
-password: process.env.DATABASE_PASSWORD ||'xRPdhlMDaycUPj2jDSwR',                         // Database password
-database: process.env.DATABASE_DATABASE_NAME ||'begeifmysnlnhnz9shw9',                     // Database name
-port: process.env.DATABASE_PORT || 3306,     
+host: process.env.DATABASE_HOST , // Database host
+user: process.env.DATABASE_USER ,                                     // Database user
+password: process.env.DATABASE_PASSWORD ,                         // Database password
+database: process.env.DATABASE_DATABASE_NAME ,                     // Database name
+port: process.env.DATABASE_PORT ,     
 connectionLimit:3 
 })
 
