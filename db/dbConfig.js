ffe51da2647f93dd4b27afb28d141993e2351e99
mul <jimpowerdire@gmail.com>
2025-01-12 12:13:04 +0300

    commit EV

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -10,7 +10,7 @@ host: process.env.DATABASE_HOST ||'begeifmysnlnhnz9shw9-mysql.services.clever-cl
 user: process.env.DATABASE_USER ||'u4bz25p8gzp7tudf',                                     // Database user
 password: process.env.DATABASE_PASSWORD ||'xRPdhlMDaycUPj2jDSwR',                         // Database password
 database: process.env.DATABASE_DATABASE_NAME ||'begeifmysnlnhnz9shw9',                     // Database name
-port: process.env.DB_PORT || 3306,     
+port: process.env.DATABASE_PORT || 3306,     
 connectionLimit:3 
 })
 
